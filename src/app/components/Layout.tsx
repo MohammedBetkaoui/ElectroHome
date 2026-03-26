d2ca9c3b3fa682bb703e1c75b1f12e5fbc2fec9e
MohammedBetkaoui <mohammed.betkaoui@example.com>
2026-03-26 23:32:52 +0100

    ameloire

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,9 +1,17 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { StoreProvider } from "../data/store";
 
+import { useEffect } from "react";
+
 export function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <StoreProvider>
       <div className="min-h-screen flex flex-col font-[DM_Sans,sans-serif]">
